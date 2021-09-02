9d058738efe55267a6593c3b4017088c4093c8dc
Faysal <74490913+faysalchowdhury99@users.noreply.github.com>
2021-09-02 09:36:22 +0600

    refactor

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const resultFoundNumberEl = document.getElementById('result-found-number');
 const booksContainer = document.getElementById('books-container');
 const spinner = `
 <div class="text-center">
-    <div class="spinner-border" role="status">
+    <div class="spinner-border text-secondary" role="status">
         <span class="visually-hidden">Loading...</span>
     </div>
 </div>
@@ -11,49 +11,59 @@ const spinner = `
 
 // Load Books
 const loadBooks = async (searchText) => {
-  resultFoundNumberEl.innerHTML = '';
-  booksContainer.innerHTML = spinner;
-  const url = `https://openlibrary.org/search.json?q=${searchText}`;
-  const res = await fetch(url);
-  const data = await res.json();
-  displayBooks(data);
+  try {
+    // display spinner and hide result found number
+    resultFoundNumberEl.innerHTML = '';
+    booksContainer.innerHTML = spinner;
+    // fetch data and display
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayBooks(data);
+  } catch (error) {
+    alert(error);
+  }
 };
 
 // Display Books
 const displayBooks = ({ docs, numFound }) => {
-  resultFoundNumberEl.innerHTML = `<strong>${numFound}</strong> Results Found!`;
+  resultFoundNumberEl.innerHTML = `
+  <strong>${numFound === 0 ? 'No' : numFound}</strong> Results Found!
+  `;
 
   booksContainer.innerHTML = '';
   docs.forEach((book) => {
-    const imageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+    const { cover_i, title, author_name, publisher, first_publish_year } = book;
+    const imageUrl = `https://covers.openlibrary.org/b/id/${cover_i}-M.jpg`;
+
     const bookDiv = document.createElement('div');
     bookDiv.className = 'col-md-4 col-lg-3';
     bookDiv.innerHTML = `
-    <div class="card">
+    <div class="card h-100">
         ${
-          book.cover_i
-            ? `<img src="${imageUrl}" class="card-img-top img-thumbnail" 
-                alt="${book.title}">`
+          cover_i
+            ? `<img src="${imageUrl}" class="card-img-top" 
+                alt="${title}">`
             : ''
         }
         <div class="card-body">
-            <h5 class="card-title"><strong>Title:</strong> ${book.title}</h5>
+            <h5 class="card-title"><strong>Title:</strong> ${title}</h5>
             ${
-              book.author_name
+              author_name
                 ? `<p class="card-text mb-2"><strong>Author:</strong> 
-                    ${book.author_name.join(', ')}</p>`
+                    ${author_name.join(', ')}</p>`
                 : ''
             }
             ${
-              book.publisher
+              publisher
                 ? `<p class="card-text mb-2"><strong>Publisher:</strong> 
-                    ${book.publisher.join(', ')}</p>`
+                    ${publisher.join(', ')}</p>`
                 : ''
             }
             ${
-              book.first_publish_year
+              first_publish_year
                 ? `<p class="card-text"><strong>First Publish:</strong> 
-                    ${book.first_publish_year}</p>`
+                    ${first_publish_year}</p>`
                 : ''
             }
         </div>
@@ -68,14 +78,16 @@ const handleSearch = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
 
-  // validate
+  // if search filed is blank
   if (searchText.trim() === '') {
     alert("Can't be empty!");
     return;
   }
 
+  // load books
   loadBooks(searchText);
 
+  // clear input
   searchField.value = '';
 };
 
